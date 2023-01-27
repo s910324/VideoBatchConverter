0b45ac5cfc4d3698096cda5777b4059c872a7d73
Fujimi no Nikaidō <scott910324@gmail.com>
2023-01-27 23:35:05 +0800

    Add files via upload
    add color tuning function, support image processing

--- videoProcessor.py
+++ videoProcessor.py
@@ -1,22 +1,51 @@
-import cv2
-import numpy as np 
+
 import os
 import sys
-from   PyQt5.Qt                 import Qt
-from   PyQt5.QtWidgets          import * 
-from   PyQt5.QtCore             import *
-from   PyQt5.QtGui              import * 
+import numpy as np
+from cv2          import cvtColor               as cv2_cvtColor
+from cv2          import split                  as cv2_split
+from cv2          import merge                  as cv2_merge
 
+from cv2          import imread                 as cv2_imread
+from cv2          import imwrite                as cv2_imwrite
+from cv2          import bitwise_not            as cv2_bitwise_not
 
-debug = False
+from cv2          import COLOR_BGR2HSV          as cv2_COLOR_BGR2HSV
+from cv2          import COLOR_HSV2BGR          as cv2_COLOR_HSV2BGR
+from cv2          import IMREAD_COLOR           as cv2_IMREAD_COLOR
+from cv2          import VideoCapture           as cv2_VideoCapture
+from cv2          import VideoWriter            as cv2_VideoWriter
+from cv2          import VideoWriter_fourcc     as cv2_VideoWriter_fourcc
 
+from cv2          import CAP_PROP_FPS           as cv2_CAP_PROP_FPS
+from cv2          import CAP_PROP_FRAME_WIDTH   as cv2_CAP_PROP_FRAME_WIDTH
+from cv2          import CAP_PROP_FRAME_HEIGHT  as cv2_CAP_PROP_FRAME_HEIGHT
+from cv2          import CAP_PROP_FRAME_COUNT   as cv2_CAP_PROP_FRAME_COUNT
+from cv2          import THRESH_BINARY          as cv2_THRESH_BINARY
+from cv2          import INTER_NEAREST          as cv2_INTER_NEAREST
+from cv2          import flip                   as cv2_flip
+from cv2          import resize                 as cv2_resize
+from cv2          import threshold              as cv2_threshold
+from cv2          import destroyAllWindows      as cv2_destroyAllWindows
+
+from PyQt5.QtCore import QObject, pyqtSignal, QCoreApplication
+
+debug = True
 
 def videoInfo(cap):
 	return {
-		"fps"        : int(cap.get(cv2.CAP_PROP_FPS)),
-		"width"      : int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)),
-		"height"     : int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)),
-		"framecount" : int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+		"fps"        : int(cap.get(cv2_CAP_PROP_FPS)),
+		"width"      : int(cap.get(cv2_CAP_PROP_FRAME_WIDTH)),
+		"height"     : int(cap.get(cv2_CAP_PROP_FRAME_HEIGHT)),
+		"framecount" : int(cap.get(cv2_CAP_PROP_FRAME_COUNT))
+	}
+
+def imageInfo(img):
+	height, width, channels = img.shape
+	return {
+		"height"     : height,
+		"width"      : width, 
+		"channels"   : channels
 	}
 
 class VideoProcessor(QObject):
@@ -25,18 +54,29 @@ class VideoProcessor(QObject):
 		super(VideoProcessor, self).__init__(parent)
 		self.run = True
 
-	def process(self, in_name, flip:bool=False, mirror:bool=False, binary:bool=False, grouping:bool=False):
-		cap           = cv2.VideoCapture(in_name) 
-		info          = videoInfo(cap)
-		outname       = in_name.split(".")[0] + f"{'_flip'if flip else ''}"+ f"{'_mirror'if mirror else ''}"+ f"{'_binary'if binary else ''}"+ f"{'_grouping'if grouping else ''}" + ".mp4"
-		fourcc        = cv2.VideoWriter_fourcc(*'mp4v')
-		deflate_size  = (int(info["width"]/2), int(info["height"]/2))
-		inflate_size  = (info["width"], info["height"])
-		out           = cv2.VideoWriter(outname,fourcc, info["fps"], (info["width"], info["height"]))
-		framecount    = info["framecount"]
-		fps           = info["fps"]
-		threshold     = 50
-		current_frame = 0
+	def process(self, in_name, flip:bool=False, mirror:bool=False, binary:bool=False, grouping:bool=False, invert:bool=False, 
+		brightness_r:float = 1, offset_r:float = 0, brightness_g:float = 1, offset_g:float = 0, brightness_b:float = 1, offset_b:float = 0):
+
+		cap              = cv2_VideoCapture(in_name) 
+		info             = videoInfo(cap)
+		outname          = in_name.split(".")[0] + f"{'_flip'if flip else ''}"+ f"{'_mirror'if mirror else ''}" + \
+			f"{'_binary'if binary else ''}"+ f"{'_grouping'if grouping else ''}"+ f"{'_inv'if invert else ''}" + \
+			f"{('_bright_r%d' % brightness_r) if brightness_r else ''}" + f"{('_contrast_r%d' % contrast_r) if contrast_r else ''}" + \
+			f"{('_bright_g%d' % brightness_g) if brightness_g else ''}" + f"{('_contrast_g%d' % contrast_g) if contrast_g else ''}" + \
+			f"{('_bright_b%d' % brightness_b) if brightness_b else ''}" + f"{('_contrast_b%d' % contrast_b) if contrast_b else ''}" + \
+			".mp4"
+		fourcc           = cv2_VideoWriter_fourcc(*'mp4v')
+		deflate_size     = (int(info["width"]/2), int(info["height"]/2))
+		inflate_size     = (info["width"], info["height"])
+		out              = cv2_VideoWriter(outname,fourcc, info["fps"], (info["width"], info["height"]))
+		framecount       = info["framecount"]
+		fps              = info["fps"]
+		threshold        = 50
+		current_frame    = 0
+
+		color_brightness = any( not(i == 1) for i in [brightness_r, brightness_g, brightness_b])
+		color_offset     = any( not(i == 0) for i in [offset_r,     offset_g,     offset_b])
+		color_process    = color_brightness or color_offset
 
 		while self.run:
 			ret, frame = cap.read()
@@ -49,17 +89,42 @@ class VideoProcessor(QObject):
 					self.status.emit(progress)
 
 				if flip:
-					frame  = cv2.flip(frame,-1)
+					frame  = cv2_flip(frame,-1)
 
 				if mirror:	
-					frame = cv2.flip(frame, 1)
+					frame = cv2_flip(frame, 1)
 
 				if binary:
-					 _, frame = cv2.threshold( frame, threshold, 255, cv2.THRESH_BINARY)
+					 _, frame = cv2_threshold( frame, threshold, 255, cv2_THRESH_BINARY)
 
 				if grouping:
-					frame = cv2.resize( frame, deflate_size, fx=0, fy=0, interpolation = cv2.INTER_NEAREST)
-					frame = cv2.resize( frame, inflate_size, fx=0, fy=0, interpolation = cv2.INTER_NEAREST)
+					frame = cv2_resize( frame, deflate_size, fx=0, fy=0, interpolation = cv2_INTER_NEAREST)
+					frame = cv2_resize( frame, inflate_size, fx=0, fy=0, interpolation = cv2_INTER_NEAREST)
+
+				if invert:
+					frame = cv2_bitwise_not(frame)
+
+				if color_process:
+					frame_b, frame_g, frame_r = cv2_split(frame)
+					
+					if not(brightness_r == 1) or not(offset_r==0):
+						frame_r = np.int16(frame_r)
+						frame_r = frame_r * brightness_r + offset_r
+						frame_r = np.clip(frame_r, 0, 255)
+						frame_r = np.uint8(frame_r)
+
+					if not(brightness_g == 1) or not(offset_g==0):
+						frame_g = np.int16(frame_g)
+						frame_g = frame_g * brightness_g + offset_g
+						frame_g = np.clip(frame_g, 0, 255)
+						frame_g = np.uint8(frame_g)
+
+					if not(brightness_b == 1) or not(offset_b==0):
+						frame_b = np.int16(frame_b)
+						frame_b = frame_b * brightness_b + offset_b
+						frame_b = np.clip(frame_b, 0, 255)
+						frame_b = np.uint8(frame_b)
+					frame = cv2_merge ((frame_b, frame_g, frame_r))
 
 				out.write(frame)
 				
@@ -67,9 +132,94 @@ class VideoProcessor(QObject):
 				break
 		if self.run:
 			self.status.emit(100.0)
+
 		cap.release()
 		out.release()
-		cv2.destroyAllWindows()
+		cv2_destroyAllWindows()
+		debugPrint("done")
+
+
+class ImageProcessor(QObject):
+	status = pyqtSignal(float)
+	def __init__(self, parent=None):
+		super(ImageProcessor, self).__init__(parent)
+		self.run = True
+
+	def process(self, in_name, flip:bool=False, mirror:bool=False, binary:bool=False, grouping:bool=False, invert:bool=False,
+		brightness_r:float = 1, offset_r:float = 0, brightness_g:float = 1, offset_g:float = 0, brightness_b:float = 1, offset_b:float = 0):
+
+		img           = cv2_imread(in_name, cv2_IMREAD_COLOR)
+		info          = imageInfo(img)
+		outname       = in_name.split(".")[0] + f"{'_flip'if flip else ''}"+ f"{'_mirror'if mirror else ''}"+ f"{'_binary'if binary else ''}"+ f"{'_grouping'if grouping else ''}"+ f"{'_inv'if invert else ''}" + ".jpg"
+
+		deflate_size  = (int(info["width"]/2), int(info["height"]/2))
+		inflate_size  = (info["width"], info["height"])
+
+		threshold     = 50
+		current_proce = 0
+
+
+		QCoreApplication.processEvents()
+		color_brightness = any( not(i == 1) for i in [brightness_r, brightness_g, brightness_b])
+		color_offset     = any( not(i == 0) for i in [offset_r,     offset_g,     offset_b])
+		color_process    = color_brightness or color_offset
+
+		total_operation  = sum([flip, mirror, binary, grouping, invert, color_process])
+
+		if flip:
+			img  = cv2_flip(img,-1)
+			current_proce = current_proce + 1
+			self.status.emit(current_proce/total_operation * 100)
+
+		if mirror:	
+			img = cv2_flip(img, 1)
+			current_proce = current_proce + 1
+			self.status.emit(current_proce/total_operation * 100)
+			
+		if binary:
+			_, img = cv2_threshold( img, threshold, 255, cv2_THRESH_BINARY)
+			current_proce = current_proce + 1
+			self.status.emit(current_proce/total_operation * 100)
+			
+		if grouping:
+			img = cv2_resize( img, deflate_size, fx=0, fy=0, interpolation = cv2_INTER_NEAREST)
+			img = cv2_resize( img, inflate_size, fx=0, fy=0, interpolation = cv2_INTER_NEAREST)
+			current_proce = current_proce + 1
+			self.status.emit(current_proce/total_operation * 100)
+			
+		if invert:
+			img = cv2_bitwise_not(img)
+			current_proce = current_proce + 1
+			self.status.emit(current_proce/total_operation * 100)
+
+		if color_process:
+			img_b, img_g, img_r = cv2_split(img)
+
+			if not(brightness_r == 1) or not(offset_r==0):
+				img_r = np.int16(img_r)
+				img_r = img_r * brightness_r + offset_r
+				img_r = np.clip(img_r, 0, 255)
+				img_r = np.uint8(img_r)
+
+			if not(brightness_g == 1) or not(offset_g==0):
+				img_g = np.int16(img_g)
+				img_g = img_g * brightness_g + offset_g
+				img_g = np.clip(img_g, 0, 255)
+				img_g = np.uint8(img_g)
+
+			if not(brightness_b == 1) or not(offset_b==0):
+				img_b = np.int16(img_b)
+				img_b = img_b * brightness_b + offset_b
+				img_b = np.clip(img_b, 0, 255)
+				img_b = np.uint8(img_b)
+			img = cv2_merge ((img_b, img_g, img_r))
+			current_proce = current_proce + 1
+			self.status.emit(current_proce/total_operation * 100)
+
+		cv2_imwrite(outname, img)
+		self.status.emit(100.0)
+
+		cv2_destroyAllWindows()
 		debugPrint("done")
 
 def debugPrint(*args, **argv): 
--- videoProcessorGUI.py
+++ videoProcessorGUI.py
@@ -1,11 +1,15 @@
 import os
 import sys
 from   PyQt5.Qt                 import Qt
-from   PyQt5.QtWidgets          import * 
-from   PyQt5.QtCore             import *
-from   PyQt5.QtGui              import * 
+from   PyQt5.QtWidgets          import QLabel, QCheckBox, QPushButton, QGridLayout, QProgressBar, QListWidget, QListWidgetItem, QWidget, QApplication, QAbstractItemView, QSlider, QSpinBox, QDoubleSpinBox
+from   PyQt5.QtCore             import QSize
+
 from   videoProcessor           import *
 from   multiprocessing          import Process
+
+video_ext = ["MOV", "WMV", "AVI", "MKV", "MPEG", "MP4"]
+image_ext = ["PNG", "JPG", "JPEG"]
+
 
 class VideoConvertWidget(QWidget):
 	def __init__(self):
@@ -13,39 +17,57 @@ class VideoConvertWidget(QWidget):
 
 		self.runState          = False
 		self.currentProcessor  = None
-		self.task_listwidget   = TestListView()
-		self.background_label  = QLabel("Drag / Drop video")
+		self.task_listwidget   = TaskListView()
+		self.background_label  = QLabel("Drag / Drop media files")
 		self.flipVideo_check   = QCheckBox("flip video")
 		self.mirrorVideo_check = QCheckBox("mirrir video")
 		self.binaryVideo_check = QCheckBox("video binary")
 		self.groupPixel_check  = QCheckBox("pixel grouping")
+		self.colorInvert_check = QCheckBox("color invert")
+		self.colorTune_check   = QCheckBox("color tuning")
+		self.colorTune_widget  = ColorTuneWidget()
 		self.execute_button    = QPushButton("execute")
 		self.delete_button     = QPushButton("clear")
 		self.float_layout      = QGridLayout()
 		self.layout            = QGridLayout()
+		self.colorTune_layout  = QGridLayout()
+
 
 		self.background_label.setAlignment(Qt.AlignCenter)
 		self.background_label.setStyleSheet("font-size: 20px; color:#777777;")
 		self.setWindowTitle("Batch Video Converter")
 		self.delete_button.clicked.connect(lambda : self.task_listwidget.clear())
 		self.task_listwidget.setDragDropMode(QAbstractItemView.DragDrop)
+		self.task_listwidget.setMinimumHeight(200)
 		uiFixedHeight = 35
 
-		self.float_layout.addWidget (self.background_label,  0, 0, 4, 4)
+		self.float_layout.addWidget (self.background_label, 0, 0, 4, 4)
 		self.float_layout.addWidget (self.task_listwidget,  0, 0, 4, 4)
 		self.float_layout.addWidget (self.delete_button,    3, 3, 1, 1)
 
-		self.layout.addLayout(self.float_layout, 0, 0, 1, 4)
-		for index, widget in enumerate([self.flipVideo_check, self.mirrorVideo_check, self.binaryVideo_check, self.groupPixel_check]):
+		self.layout.addLayout(self.float_layout, 0, 0, 1, 5)
+		for index, widget in enumerate([self.flipVideo_check, self.mirrorVideo_check, self.binaryVideo_check, self.groupPixel_check, self.colorInvert_check]):
 			self.layout.addWidget (widget,  1, index, 1, 1)
 			widget.setFixedHeight(uiFixedHeight)
-		self.layout.addWidget (self.execute_button,  2, 1, 1, 2)
+
+		self.layout.addLayout (self.colorTune_layout,   2, 0, 1, 5)
+		self.layout.addWidget (self.execute_button,     3, 1, 1, 3)
+
+		self.colorTune_layout.addWidget (self.colorTune_check,   0, 0, 1, 5)
+		self.colorTune_layout.addWidget (self.colorTune_widget,  1, 0, 1, 5)
+
 		self.delete_button.setFixedSize(45, 25)
 		self.setLayout(self.layout)
 
-
+		self.colorTune_widget.setVisible(False)
+		self.colorTune_check.stateChanged.connect(lambda val : self.colorTune_widget.setVisible(val))
+		self.colorTune_check.stateChanged.connect(lambda val : self.colorTune_widget.reset() if val else lambda:None)
 		self.execute_button.clicked.connect(self.executeProcess)
 		self.resize(500,300)
+		
+	def closeEvent(self, event):
+		self.task_listwidget.model().modelReset.disconnect()
+		event.accept()
 
 	def executeProcess(self):
 		self.setRunState(not(self.runState))
@@ -55,8 +77,15 @@ class VideoConvertWidget(QWidget):
 			mirrorVideo = self.mirrorVideo_check.isChecked()
 			binaryVideo = self.binaryVideo_check.isChecked()
 			groupPixel  = self.groupPixel_check.isChecked()
+			colorInvert = self.colorInvert_check.isChecked()
+			colorTune   = self.colorTune_widget.getValue()
 
-			p = Process(target = self.videoProcess(flipVideo, mirrorVideo, binaryVideo, groupPixel))
+			p = Process(target = self.videoProcess(
+				flip = flipVideo, mirror = mirrorVideo, binary = binaryVideo, grouping = groupPixel, invert = colorInvert,
+				brightness_r = colorTune["red_brightness"],   offset_r = colorTune["red_offset"],
+				brightness_g = colorTune["green_brightness"], offset_g = colorTune["green_offset"],
+				brightness_b = colorTune["blue_brightness"],  offset_b = colorTune["blue_offset"]
+			))
 			p.start()
 			p.join()
 
@@ -65,21 +94,35 @@ class VideoConvertWidget(QWidget):
 			if self.currentProcessor:
 				self.currentProcessor.run = False
 
-	def videoProcess(self, flipVideo, mirrorVideo, binaryVideo, groupPixel):
+	def videoProcess(self, *args, **argv):
 
 		for row in range(self.task_listwidget.count()):
 
 			if not(self.runState):
 				break
 
+
 			QCoreApplication.processEvents()
 			item                  = self.task_listwidget.item(row)
 			widget                = self.task_listwidget.itemWidget(item)
 			in_name               = widget.filePath
-			procrssor             = VideoProcessor(self)
-			self.currentProcessor = procrssor
-			procrssor.status.connect(lambda value : widget.setProgress(int(value)))
-			procrssor.process(in_name, flip = flipVideo, mirror = mirrorVideo, binary = binaryVideo, grouping = groupPixel)
+			procrssor             = None 
+
+			if in_name.split(".")[-1].upper() in video_ext:
+				procrssor = VideoProcessor(self)
+				debugPrint("Process video %s" % in_name)
+
+			if in_name.split(".")[-1].upper() in image_ext:
+				procrssor = ImageProcessor(self)
+				debugPrint("Process image %s" % in_name)
+
+			if procrssor:
+				self.currentProcessor = procrssor
+				procrssor.status.connect(lambda value : widget.setProgress(int(value)))
+				procrssor.process(in_name, *args, **argv)
+			else:
+				debugPrint("Invalid item %s" % in_name)
+
 		self.currentProcessor = None
 		self.setRunState(False)
 
@@ -87,7 +130,159 @@ class VideoConvertWidget(QWidget):
 	def setRunState(self, status):
 		self.runState = status
 		self.execute_button.setText("cancel" if status else "execute")
-		_ = [widget.setEnabled(not(self.runState)) for widget in [self.task_listwidget, self.flipVideo_check, self.binaryVideo_check, self.mirrorVideo_check, self.binaryVideo_check, self.groupPixel_check, self.delete_button]]
+		_ = [widget.setEnabled(not(self.runState)) for widget in [self.task_listwidget, self.flipVideo_check, self.binaryVideo_check, self.mirrorVideo_check, self.binaryVideo_check, self.groupPixel_check, self.colorInvert_check, self.delete_button]]
+
+class ColorTuneWidget(QWidget):
+	red_brightness_changed   = pyqtSignal(float)
+	green_brightness_changed = pyqtSignal(float)
+	blue_brightness_changed  = pyqtSignal(float)
+	red_offset_changed       = pyqtSignal(int)
+	green_offset_changed     = pyqtSignal(int)
+	blue_offset_changed      = pyqtSignal(int)
+
+	def __init__(self):
+		super().__init__()
+		self.colorTune_check        = QCheckBox("color tune")
+		self.red_brightness_slide   = QSlider()
+		self.green_brightness_slide = QSlider()
+		self.blue_brightness_slide  = QSlider()
+
+		self.red_offset_slide       = QSlider()
+		self.green_offset_slide     = QSlider()
+		self.blue_offset_slide      = QSlider()
+
+		self.red_brightness_spin    = QDoubleSpinBox()
+		self.green_brightness_spin  = QDoubleSpinBox()
+		self.blue_brightness_spin   = QDoubleSpinBox()
+
+		self.red_offset_spin        = QSpinBox()
+		self.green_offset_spin      = QSpinBox()
+		self.blue_offset_spin       = QSpinBox()
+
+		self.red_brightness_reset   = QPushButton("reset")
+		self.green_brightness_reset = QPushButton("reset")
+		self.blue_brightness_reset  = QPushButton("reset")
+
+		self.red_offset_reset       = QPushButton("reset")
+		self.green_offset_reset     = QPushButton("reset")
+		self.blue_offset_reset      = QPushButton("reset")
+
+		self.color_layout           = QGridLayout()
+
+
+		for index, label_text in enumerate([
+			"red amplify", "green amplify", "blue amplify", 
+			"red offset",  "green offset",  "blue offset"]):
+			label_widget = QLabel(label_text)
+			label_widget.setAlignment(Qt.AlignRight)
+			label_widget.setFixedWidth(70)
+			self.color_layout.addWidget (label_widget,  index,     0, 1, 1)
+
+
+		for index, widget in enumerate([self.red_brightness_slide, self.green_brightness_slide, self.blue_brightness_slide]):
+			widget.setOrientation(Qt.Horizontal)
+			widget.setMaximum (20)
+			widget.setMinimum  (0)
+			widget.setPageStep (1)
+			widget.setSliderPosition (10)
+			self.color_layout.addWidget (widget,        index,     1, 1, 3)	
+
+		for index, widget in enumerate([self.red_offset_slide,     self.green_offset_slide,     self.blue_offset_slide]):
+			widget.setOrientation(Qt.Horizontal)
+			widget.setMaximum ( 255)
+			widget.setMinimum (-255)
+			widget.setPageStep (1)
+			widget.setSliderPosition (0)
+			self.color_layout.addWidget (widget,        index + 3, 1, 1, 3)
+
+		for index, widget in enumerate([self.red_brightness_spin, self.green_brightness_spin, self.blue_brightness_spin]):
+			widget.setMaximum  (2)
+			widget.setMinimum  (0)
+			widget.setSingleStep (0.1)
+			widget.setValue (1)
+			widget.setFixedWidth(50)
+			widget.setAlignment(Qt.AlignCenter)
+			self.color_layout.addWidget (widget,        index,     4, 1, 1)	
+
+		for index, widget in enumerate([self.red_offset_spin,     self.green_offset_spin,     self.blue_offset_spin]):
+			widget.setMaximum ( 255)
+			widget.setMinimum (-255)
+			widget.setSingleStep (1)
+			widget.setValue (0)
+			widget.setFixedWidth(50)
+			widget.setAlignment(Qt.AlignCenter)
+			self.color_layout.addWidget (widget,        index + 3, 4, 1, 1)
+
+		for index, widget in enumerate([self.red_offset_spin,     self.green_offset_spin,     self.blue_offset_spin]):
+			widget.setMaximum ( 255)
+			widget.setMinimum (-255)
+			widget.setSingleStep (1)
+			widget.setValue (0)
+			widget.setFixedWidth(50)
+			widget.setAlignment(Qt.AlignCenter)
+			self.color_layout.addWidget (widget,        index + 3, 4, 1, 1)
+
+		for index, widget in enumerate([
+			self.red_brightness_reset, self.green_brightness_reset, self.blue_brightness_reset,
+			self.red_offset_reset,     self.green_offset_reset,     self.blue_offset_reset]):
+
+			widget.setFixedWidth(50)
+			self.color_layout.addWidget (widget,        index,    5, 1, 1)
+		self.setLayout(self.color_layout)
+
+
+		self.red_brightness_slide.valueChanged.connect(   lambda val: self.red_brightness_changed.emit(   round(val/10, 1)))
+		self.green_brightness_slide.valueChanged.connect( lambda val: self.green_brightness_changed.emit( round(val/10, 1)))
+		self.blue_brightness_slide.valueChanged.connect(  lambda val: self.blue_brightness_changed.emit(  round(val/10, 1)))
+		self.red_brightness_spin.valueChanged.connect(    lambda val: self.red_brightness_changed.emit(   round(val,    1)))
+		self.green_brightness_spin.valueChanged.connect(  lambda val: self.green_brightness_changed.emit( round(val,    1)))
+		self.blue_brightness_spin.valueChanged.connect(   lambda val: self.blue_brightness_changed.emit(  round(val,    1)))
+
+		self.red_brightness_changed.connect(  lambda val: self.red_brightness_slide.setValue(  int(val*10)) if not (val * 10 == self.red_brightness_slide.value())   else lambda:None )
+		self.green_brightness_changed.connect(lambda val: self.green_brightness_slide.setValue(int(val*10)) if not (val * 10 == self.green_brightness_slide.value()) else lambda:None )
+		self.blue_brightness_changed.connect( lambda val: self.blue_brightness_slide.setValue( int(val*10)) if not (val * 10 == self.blue_brightness_slide.value())  else lambda:None )
+		self.red_brightness_changed.connect(  lambda val: self.red_brightness_spin.setValue(       val    ) if not (val      == self.red_brightness_spin.value())    else lambda:None )
+		self.green_brightness_changed.connect(lambda val: self.green_brightness_spin.setValue(     val    ) if not (val      == self.green_brightness_spin.value())  else lambda:None )
+		self.blue_brightness_changed.connect( lambda val: self.blue_brightness_spin.setValue(      val    ) if not (val      == self.blue_brightness_spin.value())   else lambda:None )
+
+		self.red_offset_slide.valueChanged.connect(   lambda val: self.red_offset_changed.emit(val))
+		self.green_offset_slide.valueChanged.connect( lambda val: self.green_offset_changed.emit(val))
+		self.blue_offset_slide.valueChanged.connect(  lambda val: self.blue_offset_changed.emit(val))
+		self.red_offset_spin.valueChanged.connect(    lambda val: self.red_offset_changed.emit(val))
+		self.green_offset_spin.valueChanged.connect(  lambda val: self.green_offset_changed.emit(val))
+		self.blue_offset_spin.valueChanged.connect(   lambda val: self.blue_offset_changed.emit(val))
+
+		self.red_offset_changed.connect(  lambda val: self.red_offset_slide.setValue(  int(val)) if not (val == self.red_offset_slide.value())   else lambda:None )
+		self.green_offset_changed.connect(lambda val: self.green_offset_slide.setValue(int(val)) if not (val == self.green_offset_slide.value()) else lambda:None )
+		self.blue_offset_changed.connect( lambda val: self.blue_offset_slide.setValue( int(val)) if not (val == self.blue_offset_slide.value())  else lambda:None )
+		self.red_offset_changed.connect(  lambda val: self.red_offset_spin.setValue(   int(val)) if not (val == self.red_offset_spin.value())    else lambda:None )
+		self.green_offset_changed.connect(lambda val: self.green_offset_spin.setValue( int(val)) if not (val == self.green_offset_spin.value())  else lambda:None )
+		self.blue_offset_changed.connect( lambda val: self.blue_offset_spin.setValue(  int(val)) if not (val == self.blue_offset_spin.value())   else lambda:None )
+
+		self.red_brightness_reset.clicked.connect(lambda   : self.red_brightness_changed.emit(1.0))
+		self.green_brightness_reset.clicked.connect(lambda : self.green_brightness_changed.emit(1.0))
+		self.blue_brightness_reset.clicked.connect(lambda  : self.blue_brightness_changed.emit(1.0))
+		self.red_offset_reset.clicked.connect(lambda       : self.red_offset_changed.emit(0))
+		self.green_offset_reset.clicked.connect(lambda     : self.green_offset_changed.emit(0))
+		self.blue_offset_reset.clicked.connect(lambda      : self.blue_offset_changed.emit(0))
+
+	def reset(self):
+		self.red_brightness_changed.emit(1.0)
+		self.green_brightness_changed.emit(1.0)
+		self.blue_brightness_changed.emit(1.0)
+		self.red_offset_changed.emit(0)
+		self.green_offset_changed.emit(0)
+		self.blue_offset_changed.emit(0)
+
+	def getValue(self):
+		return {
+			"red_brightness"   : self.red_brightness_spin.value(),
+			"green_brightness" : self.green_brightness_spin.value(),
+			"blue_brightness"  : self.blue_brightness_spin.value(),
+			"red_offset"       : self.red_offset_spin.value(),
+			"green_offset"     : self.green_offset_spin.value(),
+			"blue_offset"      : self.blue_offset_spin.value()
+		}
 
 class TaskListWidget(QWidget):
 	remove = pyqtSignal(object)
@@ -122,9 +317,11 @@ class TaskListWidget(QWidget):
 		self.process_label.setText(f"{progress:d}%")
 		self.progress_bar.setValue(progress)
 
-class TestListView(QListWidget):
+
+
+class TaskListView(QListWidget):
 	def __init__(self, parent=None):
-		super(TestListView, self).__init__(parent)
+		super(TaskListView, self).__init__(parent)
 		self.setAcceptDrops(True)
 		self.setIconSize(QSize(72, 72))
 		self.setSelectionMode(QAbstractItemView.ExtendedSelection)
@@ -162,13 +359,17 @@ class TestListView(QListWidget):
 
 	def addVideoItems(self, videoPath_list):
 		for videoPath in videoPath_list:
-			if videoPath.split(".")[-1].upper() in ["MOV", "WMV", "AVI", "MKV", "MPEG", "MP4"]:
+			if (videoPath.split(".")[-1].upper() in video_ext) or (videoPath.split(".")[-1].upper() in image_ext):
+				debugPrint("add media %s" % videoPath)
 				item        = QListWidgetItem(self)
 				row         = TaskListWidget(videoPath, item)
 				row.remove.connect(lambda  i: self.takeItem (self.row(i)))
 				self.addItem(item)
 				item.setSizeHint(row.minimumSizeHint())
 				self.setItemWidget(item, row)
+			else:
+				debugPrint("add media error%s" % videoPath)
+
 
 
 if __name__ == '__main__':
